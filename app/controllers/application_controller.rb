9a917a7a1525ae83a6bc6ba0ecadd354c4383972
Nik <nik.cimino@gmail.com>
2012-10-24 07:34:48 -0600

    cleaned up the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,8 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
   helper_method :admin?, :current_cart
+  protect_from_forgery
 
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '526292d4095d0fd8135a9aa118f5956e'
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  #filter_parameter_logging :password
-  
   def current_cart
     #logger.debug "The session[:cart_id]: #{session[:cart_id]}"
     if session[:cart_id]
